72f9a2527d1d7c7835b906c869cba164e4afae0f
Lakshmi <Lakshmi@contiq.com>
2018-07-04 08:15:31 +0530

    experience and portfolio section add in my website

--- src/app/page/page.module.ts
+++ src/app/page/page.module.ts
@@ -5,13 +5,17 @@ import { PageComponent } from './page.component';
 import { PageRoutingModule } from './page-routing.module';
 import {SharedModule} from '../shared/shared.module';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
-import { SocialSectionComponent } from './social-section/social-section.component'
+import { SocialSectionComponent } from './social-section/social-section.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { PortfolioComponent } from './portfolio/portfolio.component'
 @NgModule({
   declarations: [
     HomeComponent,
     PageComponent,
     PersonalInfoComponent,
-    SocialSectionComponent
+    SocialSectionComponent,
+    ExperienceComponent,
+    PortfolioComponent
    
   ],
   imports: [
